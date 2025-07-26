fc7406017d1b68a9275bd44518c9a96ba936d46e
Ghania Ali <167546942+Ghaniaali@users.noreply.github.com>
2025-07-27 02:57:10 +0500

    Update script.js

--- Task 1/script.js
+++ Task 1/script.js
@@ -1,5 +1,4 @@
-document
-  .getElementById("registrationform").addEventListener("submit", function (event) {
+document.getElementById("registrationform").addEventListener("submit", function (event) {
     event.preventDefault();
 
     let name = document.getElementById("fname").value + " " + document.getElementById("lname").value;
@@ -18,5 +17,47 @@ document
       cnic: cnic,
     };
 
-    console.log("JSON Data:", JSON.stringify(userData, null, 2));
+    const jsonoutput = document.getElementById("jsonoutput");
+    jsonoutput.textContent = JSON.stringify(userData, null, 2);
+    document.getElementById("registrationform").reset();
+
   });
+
+   function clearform(){
+        document.getElementById("registrationform").reset();
+    }
+
+   const today = new Date().toISOString().split("T")[0];
+   document.getElementById("bdate").max = today;
+
+   document.getElementById("fname").addEventListener('keydown', function(event) {
+        if (event.key >= '0' && event.key <= '9') {
+            event.preventDefault();
+        }
+    });
+
+     document.getElementById("lname").addEventListener('keydown', function(event) {
+        if (event.key >= '0' && event.key <= '9') {
+            event.preventDefault();
+        }
+    });
+
+    document.getElementById("cnic").addEventListener('keydown', function(event) {
+        if ((event.key >= 'a' && event.key <= 'z') || (event.key >= 'A' && event.key <= 'Z')){
+            event.preventDefault();
+        }
+    });
+
+   function ValidateEmail() {
+      let email = document.forms["myform"]["emailadd"].value;
+      let pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+      if (pattern.test(email)) {
+        return true;
+      } else {
+        alert("You have entered an invalid email address");
+        return false;
+      }
+    }
+
+  
